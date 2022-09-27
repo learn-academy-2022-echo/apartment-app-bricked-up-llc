086bc45db1ca5c348e1abe8d8bb10569da476f3b
hieu <hieuhuule@gmail.com>
2022-09-27 18:07:33 -0500

    completed presence validations for street, city, state, manager, email, price, bedrooms, bathrooms, pets, and image. user_id not done

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,7 @@
 class Apartment < ApplicationRecord
   belongs_to :user
-  validates :street, presence: true
+  validates :street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, presence: true
+  validates :user_id, presence: true
+
 end
+
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Apartment, type: :model do
 
   it 'should have a valid street' do
     apartment = user.apartments.create(
+      user_id: 1,
       city: 'London', 
       state: 'England', 
       manager: 'Ms. Hudson', 
@@ -17,4 +18,165 @@ RSpec.describe Apartment, type: :model do
     )
     expect(apartment.errors[:street]).to_not be_empty 
   end
+
+  it 'should have a valid city' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '45 Candy lane', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:city]).to_not be_empty 
+  end
+
+  it 'should have a valid state' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '45 Candy lane', 
+      city: 'London', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:state]).to_not be_empty 
+  end
+
+  it 'should have a valid manager' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Bakers St',
+      city: 'London', 
+      state: 'England', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:manager]).to_not be_empty 
+  end
+
+  it 'should have a valid email' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:email]).to_not be_empty 
+  end
+
+  it 'should have a valid price' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:price]).to_not be_empty 
+  end
+
+  it 'should have a valid bedroom' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:bedrooms]).to_not be_empty 
+  end
+
+  it 'should have a valid bathrooms' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:bathrooms]).to_not be_empty 
+  end
+
+  it 'should have a valid pets' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:pets]).to_not be_empty 
+  end
+
+  it 'should have a valid image' do
+    apartment = user.apartments.create(
+      user_id: 1,
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no'
+    )
+    expect(apartment.errors[:image]).to_not be_empty 
+  end
+
+  it 'should have a valid user_id' do
+    apartment = user.apartments.create(
+      street: '3b Baker St',
+      city: 'London', 
+      state: 'England', 
+      manager: 'Ms. Hudson', 
+      email: 'mzhud@email.com', 
+      price: '1000', 
+      bedrooms: 2, 
+      bathrooms: 2, 
+      pets: 'no',
+      image: 'https://c8.alamy.com/comp/B0RJGE/small-bungalow-home-with-pathway-in-addlestone-surrey-uk-B0RJGE.jpg'
+    )
+    expect(apartment.errors[:user_id]).to_not be_empty 
+  end
+
 end
