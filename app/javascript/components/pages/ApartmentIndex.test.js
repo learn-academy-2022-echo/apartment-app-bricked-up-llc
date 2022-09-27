e21240be8efbcc75600732991675527a4d25766b
luislaurel <luislaurel133@gmail.com>
2022-09-27 17:17:13 -0600

    Unprotected index card completed, app functioning, index showing apartments

--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -1,10 +1,21 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { screen, render } from "@testing-library/react"
 import ApartmentIndex from "./ApartmentIndex"
+import { BrowserRouter } from "react-router-dom"
+import mockApartments from '../mockApartments'
 
 describe("<ApartmentIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
-    render(<ApartmentIndex />, div)
+
+    render(
+    <BrowserRouter>
+     <ApartmentIndex mockApartments={mockApartments}/>
+    </BrowserRouter>)
+
+    mockApartments.forEach(apartment => {
+      const element = screen.getByText(apartment.street)
+      expect(element).toBeInTheDocument()
+    })
   })
 })
