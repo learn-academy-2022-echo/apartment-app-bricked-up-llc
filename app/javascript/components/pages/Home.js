db19f0fe54ee7830df2e4f4e906b13aa363f2399
luislaurel <luislaurel133@gmail.com>
2022-09-28 13:03:46 -0600

    Added conditional rendering, need create listings buttons.

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,9 +1,31 @@
 import React from "react"
-
-const Home = () => {
+import { Button, NavLink, NavItem} from "reactstrap"
+const Home = (props) => {
   return (
     <>
       <h3>Home</h3>
+      {props.logged_in && (
+        <>
+         <h3>Welcome!</h3>
+         <NavItem>
+          <Button>
+            <a href={props.new_user_route} className="nav-link">
+              Please Sign in
+            </a>
+            </Button>
+          </NavItem>
+          </>
+        )}
+        {!props.logged_in && (
+          
+         <NavItem>
+          <Button>
+            <a href={props.new_user_route} className="nav-link">
+              Please Sign in
+            </a>
+            </Button>
+          </NavItem>
+        )}
     </>
   )
 }
