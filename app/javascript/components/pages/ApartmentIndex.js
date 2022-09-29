608b9516893461cd2f782300377dc39af7b8f17f
sanjuel <samueljscott101@gmail.com>
2022-09-29 10:47:26 -0700

    added create functionality

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,55 +1,55 @@
-import React from "react"
-import {Card, Button, CardBody,CardText, CardTitle, ListGroup, ListGroupItem, CardLink} from "reactstrap"
-import {NavLink} from 'react-router-dom'
+import React from "react";
+import {
+  Card,
+  Button,
+  CardBody,
+  CardText,
+  CardTitle,
+  ListGroup,
+  ListGroupItem,
+  CardLink,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
-const ApartmentIndex = ({apartments}) => {
-  console.log(apartments)
+const ApartmentIndex = ({ apartments }) => {
+  console.log(apartments);
   return (
     <div>
-    {apartments.map((apartment)=> {
-      return ( 
-        <>
-        <Card style={{width: '18rem'}}>
-          <img alt="Card" src={apartment.image}/>
-          <CardBody>
-            <CardTitle tag="h5">
-              {apartment.street}
-            </CardTitle>
-            <CardText>
-              {apartment.city}
-            </CardText>
-            </CardBody>
-          <ListGroup flush>
-          <ListGroupItem>
-                  {apartment.state}
-          </ListGroupItem>
-          <ListGroupItem>
-                  {apartment.price}
-          </ListGroupItem>
-          <ListGroupItem>
+      <h1>All Listings</h1>
+      {apartments.map((apartment) => {
+        return (
+          <>
+            <Card style={{ width: "18rem" }}>
+              <img alt="Card" src={apartment.image} />
+              <CardBody>
+                <CardTitle tag="h5">{apartment.street}</CardTitle>
+                <CardText>{apartment.city}</CardText>
+              </CardBody>
+              <ListGroup flush>
+                <ListGroupItem>{apartment.state}</ListGroupItem>
+                <ListGroupItem>{apartment.price}</ListGroupItem>
+                <ListGroupItem>
                   {apartment.bedrooms}
                   {apartment.bathrooms}
-          </ListGroupItem>
-          </ListGroup>
-            <CardBody>
-                <CardLink >
-                <NavLink to={`/apartmentshow/${apartment.id}`} className="nav-link">
-                    <Button>
-                    Find Out More
-                    </Button>
-                </NavLink>
+                </ListGroupItem>
+              </ListGroup>
+              <CardBody>
+                <CardLink>
+                  <NavLink
+                    to={`/apartmentshow/${apartment.id}`}
+                    className="nav-link"
+                  >
+                    <Button>Find Out More</Button>
+                  </NavLink>
                 </CardLink>
-                <CardLink href="#">
-                  Another Card Link
-                </CardLink>
-            </CardBody>
-         </Card>
-         </>
-      )
-    })
-  }
-  </div>
-  )
-}
+                <CardLink href="#">Another Card Link</CardLink>
+              </CardBody>
+            </Card>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
-export default ApartmentIndex
+export default ApartmentIndex;
